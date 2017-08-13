b43850c9f8c05640cc8feae0a4bc64979a8cb1ba
Uwe Riegel <uwriegel@gmail.com>
2017-08-13 19:43:18 +0200

    successfully called callback

--- node.cpp
+++ node.cpp
@@ -3,19 +3,24 @@
 #include "filesystem.h"
 using namespace v8;
 
-void Method(const FunctionCallbackInfo<Value>& args) {
+void method_get_drives(const FunctionCallbackInfo<Value>& args) {
 	auto isolate = Isolate::GetCurrent();
 	auto scope(isolate);
+	auto callback = Local<Function>::Cast(args[0]);
 
 	auto drives = get_drives();
-
-	args.GetReturnValue().Set(String::NewFromTwoByte(isolate, 
-		reinterpret_cast<const uint16_t*>(drives[0].description.c_str())));
+	for (auto it = drives.begin(); it < drives.end(); it++)
+	{
+		const unsigned argc = 1;
+		Local<Value> argv[argc] = { String::NewFromTwoByte(isolate,
+			reinterpret_cast<const uint16_t*>(it->description.c_str())) };
+		callback->Call(isolate->GetCurrentContext()->Global(), argc, argv);
+	}
 }
 
 void Init(Handle<Object> exports) {
 	auto isolate = Isolate::GetCurrent();
-	exports->Set(String::NewFromUtf8(isolate, "hello"), FunctionTemplate::New(isolate, Method)->GetFunction());
+	exports->Set(String::NewFromUtf8(isolate, "getDrives"), FunctionTemplate::New(isolate, method_get_drives)->GetFunction());
 }
 
-NODE_MODULE(hello, Init)
+NODE_MODULE(filesystem, Init)
--- tester.cpp
+++ tester.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
 	auto drives = get_drives();
-	for (auto it = drives.cbegin(); it < drives.cend(); it++)
+	for (auto it = drives.begin(); it < drives.end(); it++)
 		wcout << it->name << L" - " << static_cast<int>(it->type) << L" - " << it->description << endl;
 
 	wcout << L"Hällo Welt" << endl;
