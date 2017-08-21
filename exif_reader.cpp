8c37c0f376d7328ede16e4c9e08e830c918eba4d
Uwe Riegel <uwriegel@gmail.com>
2017-08-21 23:49:54 +0200

    exif header

--- exif_reader.cpp
+++ exif_reader.cpp
@@ -18,6 +18,40 @@ bool Exif_reader::initialize()
 		return false;
 	if (!read_to_exif_start())
 		return false;
+	
+	// The next 4 bytes are the size of the Exif data.
+	auto length = read_ushort();
+	vector<char> bytes(length);
+	exif_stream.read(bytes.data(), bytes.size());
+
+	// Next is the Exif data itself. It starts with the ASCII "Exif" followed by 2 zero bytes.
+	string exif(bytes.data(), 4);
+	if (exif != "Exif")
+		return false;
+
+	// 2 zero bytes
+	if (bytes[4] != 0 || bytes[5] != 0)
+		//throw new Exception("Malformed Exif data");
+		return false;
+
+	// We're now into the TIFF format
+	auto tiff_header_start = 6;
+
+	// What byte align will be used for the TIFF part of the document? II for Intel, MM for Motorola
+	string ii(bytes.data() + 6, 2);
+	is_little_endian = ii == "II";
+
+	// Next 2 bytes are always the same.
+	if (read_ushort(bytes, 8) != 0x002A)
+		//throw new Exception("Error in TIFF data");
+		return false;
+
+	// Get the offset to the IFD (image file directory)
+	uint32_t ifd_offset = read_uint(bytes, 10);
+
+	// Note that this offset is from the first byte of the TIFF header. Jump to the IFD.
+	auto position = ifd_offset + tiff_header_start;
+	check_ifd(bytes, position);
 	return true;
 }
 
@@ -46,6 +80,11 @@ bool Exif_reader::read_to_exif_start()
 	return true;
 }
 
+void Exif_reader::check_ifd(const vector<char>& bytes, int start_position)
+{
+	auto count = read_ushort(bytes, start_position);
+}
+
 uint8_t Exif_reader::read_byte()
 {
 	char ret{ 0 };
@@ -60,4 +99,16 @@ uint16_t Exif_reader::read_ushort()
 	return htons(ret);
 }
 
+uint16_t Exif_reader::read_ushort(const vector<char>& buffer, int offset)
+{
+	uint16_t ret;
+	memcpy(&ret, buffer.data() + offset, 2);
+	return is_little_endian? ret : htons(ret);
+}
 
+uint32_t Exif_reader::read_uint(const vector<char>& buffer, int offset)
+{
+	uint32_t ret;
+	memcpy(&ret, buffer.data() + offset, 4);
+	return is_little_endian ? ret : htons(ret);
+}
--- exif_reader.h
+++ exif_reader.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <vector>
 #include <iostream>
 #include <fstream>
 using namespace std;
@@ -17,4 +18,11 @@ private:
 	bool read_to_exif_start();
 	uint8_t read_byte();
 	uint16_t read_ushort();
+
+	uint16_t read_ushort(const vector<char>& buffer, int offset);
+	uint32_t read_uint(const vector<char>& buffer, int offset);
+
+	void check_ifd(const vector<char>& bytes, int start_position);
+
+	bool is_little_endian{ false };
 };
